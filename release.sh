41e8f496a613bdd0638d46ea035cde2ea0a0af6d
weipeng <weipeng618@qq.com>
2025-01-13 11:06:18 +0800

    自动化部署

--- release.sh
+++ release.sh
@@ -6,18 +6,20 @@ echo "第一个参数是密码：$1"
 echo "第二个参数是启动哪些服务：mysql8,rust,all：$2"
 
 # 修改环境变量，并且保存（.env）
-sed -i "s/^DATABASE_URL=.*DATABASE_URL=mysql://root:$1@mysql8:3306/knowledge" .env
-sed -i "s/^MYSQL_ROOT_PASSWORD=.*MYSQL_ROOT_PASSWORD=$1" .env
+sed -i "0,/^DATABASE_URL=.*/s|^DATABASE_URL=.*|DATABASE_URL=mysql://root:$1@mysql8:3306/knowledge|" .env
+
+
+sed -i "0,/^MYSQL_ROOT_PASSWORD=.*/s|^MYSQL_ROOT_PASSWORD=.*|MYSQL_ROOT_PASSWORD=$1|" .env
 
 # 构建部署（根据传入值来确定部署哪些容器）
 if [[ $2 = "mysql8" ]]; then
   echo "1开始部署$2！"
-  docker compose -d --build mysql8
+  docker compose up -d --build mysql8
 elif [[ $2 = "rust" ]]; then
   echo "2开始部署$2！"
-  docker compose -d --build knowledge-base
+  docker compose up -d --build knowledge-base
 else
   echo "3开始部署$2！"
-  docker compose -d --build
+  docker compose up -d --build
 fi
 echo "操作完成！"
